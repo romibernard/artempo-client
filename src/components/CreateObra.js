157cb8e169a9fe78fe64f93f648a4f33f9bd3aa8
Romina <romibernard68@gmail.com>
2021-10-21 08:48:04 -0500

    form createObra add materials

--- src/components/CreateObra.js
+++ src/components/CreateObra.js
@@ -130,16 +130,19 @@ export default function CreateObra() {
                             </div>
 
                             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                                <label htmlFor="about" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                <label htmlFor="materials" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                     Materiales
                                 </label>
                                 <div className="mt-1 sm:mt-0 sm:col-span-2">
                                     <textarea
                                         id="materials"
                                         name="materials"
+                                        value={newObra.materials}
                                         rows={2}
+                                        onChange={(e) => {
+                                            handleForm(e);
+                                        }}
                                         className="max-w-lg shadow-sm block w-full focus:ring-turquoise focus:border-cerulean sm:text-sm border border-gray-300 rounded-md"
-                                        defaultValue={''}
                                     />
                                 </div>
                             </div>
@@ -148,6 +151,13 @@ export default function CreateObra() {
                                 <label htmlFor="picturesUrl" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                                     Fotografía de tu obra
                                 </label>
+                                <input value={newObra.picturesUrl}
+                                    name="picturesUrl"
+                                    onChange={(e) => {
+                                        handleForm(e)
+                                    }}
+                                    className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                />
                                 <div className="mt-1 sm:mt-0 sm:col-span-2">
                                     <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                                         <div className="space-y-1 text-center">
@@ -171,7 +181,8 @@ export default function CreateObra() {
                                                     className="relative cursor-pointer bg-white rounded-md font-medium text-blue-700 hover:text-cerulean focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-turquoise"
                                                 >
                                                     <span>Upload a file</span>
-                                                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                                    <input id="file-upload" name="file-upload" type="file" className="sr-only"
+                                                    />
                                                 </label>
                                                 <p className="pl-1">or drag and drop</p>
                                             </div>
@@ -186,7 +197,7 @@ export default function CreateObra() {
 
                 <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
                     <div>
-                        <h3 className="text-lg leading-6 font-medium text-gray-900 text-center">DIMENSIONES:</h3>
+                        <h3 className="text-lg leading-6 font-medium text-gray-900 text-center">DIMENSIONES (cm):</h3>
                         <h4 className="text-lg leading-6 font-medium text-gray-700 text-center">Recuerda solo poner los números (no letras ni caracteres especiales)</h4>
                     </div>
                     <div className="space-y-6 sm:space-y-5">
